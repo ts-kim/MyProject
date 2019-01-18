62d2a6b820c9f3bf8e7b988a0546d92e3535b653
ts-kim <zkm1989@naver.com>
2019-01-18 17:06:52 +0900

    Add Automatic dataset unzipping

--- download.sh
+++ download.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 mkdir dataset
 kaggle competitions download humpback-whale-identification -p dataset/
+mkdir dataset/test
+mkdir dataset/train
+mv dataset/train.zip dataset/train/.
+mv dataset/test.zip dataset/test/.
+unzip dataset/train/train.zip
+rm dataset/train/train.zip
+unzip dataset/test/test.zip
+rm datset/test/test.zip
+
